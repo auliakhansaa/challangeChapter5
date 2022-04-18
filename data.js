7c3e412e70304805c5176d516aca8affcea96c82
auliakhansaaa <38196380+auliakhansaaa@users.noreply.github.com>
2022-04-18 22:27:15 +0700

    last commit

---
+++ data.js
@@ -0,0 +1,14 @@
+const data = [
+    {
+      id: 1,
+      username: "admin",
+      password: "password",
+    },
+    {
+      id: 2,
+      username: "user",
+      password: "password",
+    },
+  ];
+  
+  module.exports = data;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const logins = require("./data");
 
 const app = express();
 const port = 8080;
@@ -12,6 +13,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
+app.post("/api/login", (req, res) => {
+  // Destructuring
+  const { user, pass } = req.body;
+
+  let obj = logins.find(o => o.username === user && o.password === pass);
+
+  if (obj){
+      res.status(200).json({
+      message: `Login Success`,
+    });
+  } else {
+    res.status(500).json({
+      message: `Login failed`,
+    });
+  }
+
+});
+
+app.get("/api/login/:id",(req,res)=>{
+  const login = logins.find((e) => e.id === Number(req.params.id));
+  res.status(200).json(login);
+})
+
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -20,45 +44,7 @@ app.get("/game", (req, res) => {
   res.render("game");
 });
 
-app.get("/salam", (req, res) => {
-  const namaDariQuery = req.query.nama || "Tidak ada nama";
 
-  res.render("salam", {
-    nama: namaDariQuery,
-  });
-});
-
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-
-app.post("/register", (req, res) => {
-  const judulDrama = req.body.judulDrama;
-  const genre = req.body.password;
-
-  users.push({
-    email: email,
-    password: password,
-  });
-
-  console.log(users);
-
-  res.redirect("/tampilkan-user");
-});
-
-app.get("/jumlah-user", (req, res) => {
-  res.send(`Jumlah user ${users.length}`);
-});
-
-app.get("/tampilkan-user-json", (req, res) => {
-  res.json(users);
-});
-
-app.get("/tampilkan-user", (req, res) => {
-  res.render("users", {
-    users,
-  });
-});
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
